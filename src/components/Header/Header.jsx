0a6e8763cb3c6d6880191c725646c7b0dc15a80e
Manifest-Son <lennoxgithinji37@gmail.com>
2024-07-31 10:29:32 +0300

    This creats the header section feature and makes it resposive to medium and large screens.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css'
+
+function Header(){
+    return(
+        <React.Fragment> 
+            <section>
+                <div className="header-container">
+                    <h1>Pre-Eminent Consulting</h1>
+                    <nav>
+                        <ol className='nav_links'>
+                            <li>Home</li>
+                            <li>Services</li>
+                            <li>Schedule</li>
+                            <li>Gallery</li>
+                            <li>Contact Us</li>
+                        </ol>
+                    </nav>
+                    <button>Get in Touch</button>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default Header;
