e78f5b27aa65ca8a23c79c1163f21d1be239e427
Manifest-Son <lennoxgithinji37@gmail.com>
2024-08-01 14:18:39 +0300

    This adds the about us section feature and makes it responsive in every screen.

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import './Home.css'
-import studentImg from '../../assets/student.png'
-import studentsImg from '../../assets/students.png'
-import graduateImg from '../../assets/graduste.png'
+import React from "react";
+import "./Home.css";
+import studentImg from "../../assets/student.png";
+import studentsImg from "../../assets/students.png";
+import graduateImg from "../../assets/graduate.png";
 
-function About(){
-    return(
-        <React.Fragment>
-            <section>
-                <div className="about_container">
-                    <div className="about_img">
-                    <img src={studentImg} alt="Fresh man" />
-                    <img src={studentsImg} alt="students in a group" />
-                    <img src={graduateImg} alt="Graduation ceremony" />
-                    </div>
-                    <div>
-                        <h1>About Us</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi beatae praesentium iste illum, explicabo impedit cum autem alias quo quia ullam dolorum eaque itaque.</p>
-                        <button>Learn More &rarr;</button>
-                    </div>
-                </div>
-            </section>
-        </React.Fragment>
-    )
+function About() {
+  return (
+    <React.Fragment>
+      <section>
+        <div className="about_container">
+          <div className="about_img">
+            <img src={studentImg} alt="Fresh man" className="img_1" />
+            <img
+              src={graduateImg}
+              alt="Graduation ceremony"
+              className="img_3"
+            />
+            <img
+              src={studentsImg}
+              alt="students in a group"
+              className="img_2"
+            />
+          </div>
+          <div className="about_us_txt">
+            <h1>About Us</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              sed accusantium similique officiis dolorum sunt aperiam, tenetur,
+              commodi consequuntur nulla non expedita perferendis pariatur
+              libero inventore distinctio qui consequatur repellat.
+            </p>
+            <button>Learn More &rarr;</button>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import "./Home.css";
 import React from "react";
 import Hero from "./Hero";
 import Why from "./Why";
+import About from "./About";
 
 function Home() {
   return (
     <React.Fragment>
       <Hero />
       <Why />
+      <About />
     </React.Fragment>
   );
 }
