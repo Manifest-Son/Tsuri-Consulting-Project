53191f028dfc6c2114a89051e16576a0e8bcc26f
Manifest-Son <lennoxgithinji37@gmail.com>
2024-08-02 08:02:55 +0300

    This implements the contacts page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import "./Contact.css";
+import { BsSendFill } from "react-icons/bs";
 
 function Contact() {
   return (
     <React.Fragment>
-      <h1>Hello World!</h1>
+      <section>
+      <div className="contact_header"><h1>Contact Us</h1></div>
+        <div className="contact_container">
+          <form>
+          <h1>Reach out to us...</h1>
+            <div className="form_contents">
+            <div>
+            <label htmlFor="">First Name:</label>
+            <input type="text" name='fname' id="fname" />
+            </div>
+            <div>
+            <label htmlFor="">Last Name:</label>
+            <input type="text" name='lname' id="lname" />
+            </div>
+            </div>
+            <div className="form_contents">
+            <div>
+            <label htmlFor="phoneno">Phone Number:</label>
+            <input type="text" name="phoneno" id='phoneno' /></div>
+            <div>
+            <label htmlFor="">Email Address:</label>
+            <input type="email" name="email" id="email" />
+            </div>
+            </div>
+            <textarea name="message" id="message"placeholder="Enter your message here."></textarea>
+            <button><BsSendFill />Send</button>
+          </form>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Gallery.css";
+import graduateImg from '../../assets/graduate.png'
 
 function Gallery() {
   return (
     <React.Fragment>
-      <h1>Hello World!</h1>
+        <section>
+        <div className="gallery_header"><h1>Gallery</h1></div>
+            <div className="gallery_container">
+                <div className="gallery_wrapper">
+                    <div className="gallery_info">
+                        <img src={graduateImg} alt="" />
+                        <h1>Title</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dicta perferendis exercitationem cum ipsum facere aperiam in similique rerum sit!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </React.Fragment>
   );
 }
