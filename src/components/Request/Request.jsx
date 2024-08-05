090594c9cf4019af0424a93ee494f5b751f1807e
Manifest-Son <lennoxgithinji37@gmail.com>
2024-08-05 14:38:06 +0300

    This adds validation to forms and are ready to use API's for complete functionality.

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 import "./Request.css";
 import { BsSendFill } from "react-icons/bs";
+import { useFormik } from "formik";
+import * as Yup from 'yup'
+
 
 export default function Request() {
+  const validationSchema = Yup.object({
+    firstname: Yup.string().required("Please enter your name"),
+    lastname: Yup.string().required("Please enter your last name"),
+    phoneno: Yup.string().required("Please enter phone number"),
+    emailAddress: Yup.string().required("Please enter your email address"),
+    purpose: Yup.string().required("Please enter the service you would require from us."),
+    selecteddate: Yup.string().required("Please select the date would would need us to come.")
+  })
+
+  const initialValues = ({
+    firstname: '',
+    lastname: '',
+    phoneno: '',
+    emailAddress: '',
+    purpose: '',
+    selecteddate: ''
+  })
+
+  const onSubmit = () => {
+
+  }
+
+  const formik = useFormik({initialValues, validationSchema, onSubmit})
   return (
     <React.Fragment>
       <section>
         <div className="request_container">
           <div className="request_intro">
-            <h1>Request Introduction</h1>
+            <h1>Book Us</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolor
               provident minima molestias non exercitationem! Nemo, alias itaque
@@ -16,34 +42,34 @@ export default function Request() {
             </p>
           </div>
           <div className="form_section">
-            <form>
+            <form onSubmit={formik.handleSubmit}>
               <div className="details">
                 <div className="form_input">
                   <label htmlFor="">First Name:</label>
-                  <input type="text" name="fname" id="fname" />
+                  <input type="text" name="firstname" id="firstname" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.firstname} />
                 </div>
                 <div className="form_input">
                   <label htmlFor="">Last Name:</label>
-                  <input type="text" name="lname" id="lname" />
+                  <input type="text" name="lastname" id="lastname" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.lastname}/>
                 </div>
               </div>
               <div className="details">
                 <div className="form_input">
                   <label htmlFor="">Phone No:</label>
-                  <input type="text" name="phoneno" id="phoneno" />
+                  <input type="text" name="phoneno" id="phoneno" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.phoneno}/>
                 </div>
                 <div className="form_input">
                   <label htmlFor="">Email Address:</label>
-                  <input type="email" name="fname" id="fname" />
+                  <input type="email" name="emailAddress" id="emailAddress" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.emailAddress}/>
                 </div>
               </div>
               <div className="form_input">
                 <label htmlFor="">When you need us to come</label>
-                <input type="date" name="fname" id="fname" />
+                <input type="date" name="selectdate" id="selectdate" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.selecteddate} />
               </div>
               <div className="form_input">
                 <label htmlFor="purpose">What would you want us to do?</label>
-                <select name="purpose" id="purpose">
+                <select name="purpose" id="purpose" onBlur={formik.handleBlur} onChange={formik.handleChange} value = {formik.values.purpose}>
                   <option value="none">--option--</option>
                   <option value="career_guidance">Career Guidance</option>
                   <option value="academic_performance">
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Contact.css";
 import { BsSendFill } from "react-icons/bs";
+import { useFormik } from "formik";
+import * as Yup from 'yup'
 
 function Contact() {
+  const validationSchema = Yup.object({
+    firstname: Yup.string().required("Please enter your first name"),
+    lastname: Yup.string().required("Please enter your last name"),
+    phoneno: Yup.string().required("Please enter your phone number"),
+    emailAddress: Yup.string().required("Please enter your email address"),
+    message: Yup.string().required("Please enter your message")
+  })
+
+  const initialValues = ({
+    firstname: '',
+    lastname: '',
+    phoneno: '',
+    emailAddress: '',
+    message: ''
+  })
+
+  const onSubmit = () => {
+
+  }
+
+  const formik = useFormik({initialValues, validationSchema, onSubmit})
   return (
     <React.Fragment>
       <section>
@@ -10,26 +33,32 @@ function Contact() {
           <h1>Contact Us</h1>
         </div>
         <div className="contact_container">
-          <form className="form_container">
+          <form onSubmit={formik.handleSubmit} className="form_container">
             <h1>Reach out to us...</h1>
             <div className="form_contents">
               <div className="form_details">
                 <label htmlFor="">First Name:</label>
-                <input type="text" name="fname" id="fname" />
+                <input type="text" name="firstname" id="firstname" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.firstname}/>
+                {formik.touched.firstname && formik.errors.firstname && (<p>{formik.errors.firstname}</p>)}              
               </div>
               <div className="form_details">
                 <label htmlFor="">Last Name:</label>
-                <input type="text" name="lname" id="lname" />
+                <input type="text" name="lastname" id="lastname" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.lastname}/>
+                {formik.touched.lastname && formik.errors.lastname && (<p>{formik.errors.lastname}</p>)}              
               </div>
             </div>
             <div className="form_contents">
               <div className="form_details">
                 <label htmlFor="phoneno">Phone Number:</label>
-                <input type="text" name="phoneno" id="phoneno" />
+                <input type="text" name="phoneno" id="phoneno" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.phoneno}/>
+                {formik.touched.phoneno && formik.errors.phoneno && (<p>{formik.errors.phoneno}</p>)}              
+
               </div>
               <div className="form_details">
                 <label htmlFor="">Email Address:</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="emailAddress" id="emailAddress" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.emailAddress}/>
+                {formik.touched.emailAddress && formik.errors.emailAddress && (<p>{formik.errors.emailAddress}</p>)}              
+
               </div>
             </div>
             <div className="form_details">
@@ -37,12 +66,14 @@ function Contact() {
               <textarea
                 name="message"
                 id="message"
-                placeholder="Enter your message here."
+                onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.message}
               ></textarea>
+                {formik.touched.message && formik.errors.message && (<p>{formik.errors.message}</p>)}              
+
             </div>
-            <button className="form_details">
+            <button type="submit" className="form_details">
               <BsSendFill />
-              Send
+              Send 
             </button>
           </form>
         </div>
