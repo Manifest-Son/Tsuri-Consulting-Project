2f505ecc606a24bf7561322350b6b4c0a278e01e
Lennox Githinji <114780470+Manifest-Son@users.noreply.github.com>
2024-08-12 15:47:55 +0300

    Upadting imports in Update page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Services.css";
 import leadershipImg from "../../assets/leadership.jpg";
-import academicsImg from "../../assets/academics.jpg";
+import academicsImg from "../../assets/Academics.jpg";
 import careerImg from "../../assets/career.jpg";
 import { Link } from "react-router-dom";
 import Book from "./Book";
